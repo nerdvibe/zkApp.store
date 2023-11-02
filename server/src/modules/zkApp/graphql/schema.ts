c1ed4f3b8e873c7479fce36cfb9cf1702aaf48e2
nerdvibe <6796782+nerdvibe@users.noreply.github.com>
2023-11-01 20:21:28 -0800

    added zkApp GQL schema

---
+++ server/src/modules/zkApp/graphql/schema.ts
@@ -0,0 +1,71 @@
+export const schema = `
+  input CreateZkApp {
+    name: String!
+    slug: String!
+    currentVersion: String!
+    url: String!
+    subtitle: String
+    body: String
+    discordUrl: String
+    githubUrl: String
+    category: String
+    icon: String
+    bannerImage: String
+  }
+  input updateZkApp {
+    id: String!
+    name: String
+    slug: String
+    currentVersion: String
+    url: String
+    subtitle: String
+    body: String
+    discordUrl: String
+    githubUrl: String
+    category: String
+    icon: String
+    bannerImage: String
+  }
+
+  type ZkApp {
+    id: String!
+    name: String!
+    slug: String!
+    subtitle: String
+    owner: String!
+    body: String
+    reviewScore: Float
+    reviewCount: Float
+    currentVersion: String!
+    url: String!
+    discordUrl: String
+    githubUrl: String
+    category: String
+    icon: String
+    bannerImage: String
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Token {
+    accessToken: String!
+    refreshToken: String!
+  }
+  type Message {
+    message: String!
+    error: Boolean
+  }
+
+  type Query {
+    zkApp(slug: String!): ZkApp @rateLimit(limit: 3, duration: 60)
+  }
+
+
+  type Mutation {
+    createZkApp(zkApp: CreateZkApp!): ZkApp! @rateLimit(limit: 15, duration: 60)
+    updateZkApp(zkApp: updateZkApp!): ZkApp! @rateLimit(limit: 15, duration: 60)
+    deleteZkApp(id: String!): Boolean! @rateLimit(limit: 15, duration: 60)
+    
+    checkSlug(slug: String!): Boolean! @rateLimit(limit: 15, duration: 60) 
+  }
+`;
