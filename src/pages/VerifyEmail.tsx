9b40d1e72e7a941cf7e71755b10992e110882350
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2023-12-06 12:04:45 +0100

    Fixed email verification feedback

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,36 +1,52 @@
 import { Link, useParams } from "react-router-dom";
 import { useVerifyEmailMutation } from "../gql/generated";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { Card, CardBody } from "@nextui-org/react";
-import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { Player } from "@lottiefiles/react-lottie-player";
 import paperPlane from "@/assets/animations/paper-plane.json";
 import emailConfirmed from "@/assets/animations/email-confirmed.json";
 import warning from "@/assets/animations/warning.json";
 import routes from "../routes";
 
 export default function VerifyEmail() {
+  const [loader, setLoader] = useState(true);
+  const [showError, setShowError] = useState(false);
   const { verifyEmailToken } = useParams();
-  const [verifyEmail, { called, loading, error }] = useVerifyEmailMutation();
+  const [verifyEmail, { called, loading, error, data }] =
+    useVerifyEmailMutation();
 
   useEffect(() => {
     if (verifyEmailToken && !called) {
-      toast.promise(
-        verifyEmail({
-          variables: {
-            emailVerificationToken: verifyEmailToken,
-          },
-        }),
-        {
-          loading: "Verifying email",
-          success: <b>Email verified!</b>,
-          error: (
-            <b>Cannot verify you email, please check the link and retry.</b>
-          ),
-        }
-      );
+      verifyEmail({
+        variables: {
+          emailVerificationToken: verifyEmailToken,
+        },
+      });
     }
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      const loadingToast = toast.loading("Verifying email");
+      if (data.verifyEmail?.message === "Token not valid") {
+        setShowError(true);
+        setTimeout(() => {
+          toast.dismiss(loadingToast);
+          toast.error(
+            <b>Cannot verify you email, please check the link and retry.</b>
+          );
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          toast.success(<b>Email verified!</b>);
+        }, 1000);
+      }
+      setTimeout(() => {
+        setLoader(false);
+      }, 1000);
+    }
+  }, [data]);
 
   return (
     <div className="flex flex-col gap-4 my-11 md:mx-8">
@@ -40,7 +56,7 @@ export default function VerifyEmail() {
       <div className="flex justify-center items-center h-full min-h-[500px]">
         <Card>
           <CardBody className="text-center gap-4 max-w-[400px] flex justify-center">
-            {loading ? (
+            {loader || loading ? (
               <div className="flex items-center flex-col gap-5 p-4">
                 <Player
                   src={paperPlane}
@@ -55,13 +71,13 @@ export default function VerifyEmail() {
                   </p>
                 </div>
               </div>
-            ) : error ? (
+            ) : showError || error ? (
               <div className="flex items-center flex-col gap-5 p-4 pt-0">
                 <Player
                   src={warning}
-                  loop
                   style={{ width: "100%", maxWidth: "200px" }}
                   autoplay
+                  keepLastFrame
                 />
                 <div className="mt-4 gap-2">
                   <h1 className="text-4xl text-white font-bold">Oh no!</h1>
