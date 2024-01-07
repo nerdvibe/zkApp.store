6599633661560d7ed6c5d65aefeff6a44d572632
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2024-01-07 14:21:12 +0100

    Updated .gitignore, updated footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,50 +3,56 @@ import Logo from "@/assets/logo-white.svg";
 import "./style.css";
 import routes from "@/routes";
 import { Link } from "react-router-dom";
+import {
+  API_LINK,
+  DISCORD_LINK,
+  DOCS_LINK,
+  GITHUB_LINK,
+  SUPPORT_LINK,
+  TELEGRAM_LINK,
+  ZK_IGNITE_LINK,
+} from "@/util/links";
 
 export default function Footer() {
   return (
-    <div className="flex flex-row justify-between px-5 flex-wrap items-center py-4">
+    <div className="flex flex-row justify-between px-5 flex-wrap items-center py-4 my-4">
       <div className="flex flex-col flex-1 gap-10 min-w-[300px] px-8">
         <div>
           <Image src={Logo} alt="Logo" radius="none" className="sidebar-icon" />
         </div>
       </div>
-      <div className="flex flex-row flex-1 justify-center gap-10 min-h-[160px] items-end min-w-[300px]">
+      <div className="flex flex-row flex-1 justify-center gap-10 items-start min-w-[300px]">
         <div className="flex flex-col">
           Learn
+          <a target="_blank" className="footer-link" href={API_LINK}>
+            API
+          </a>
+          <a target="_blank" className="footer-link" href={DOCS_LINK}>
+            Docs
+          </a>
           <Link className="footer-link" to={routes.PRIVACY}>
             Privacy Policy
           </Link>
-          <a className="footer-link" href="http://google.com">
-            Terms of Service
-          </a>
-          <a className="footer-link" href="http://google.com">
-            Something
-          </a>
         </div>
         <div className="flex flex-col">
-          Company
-          <a className="footer-link" href="http://google.com">
-            Careers
+          Project
+          <a target="_blank" className="footer-link" href={GITHUB_LINK}>
+            Github
           </a>
-          <a className="footer-link" href="http://google.com">
-            Help Center
-          </a>
-          <a className="footer-link" href="http://google.com">
-            Get in touch
+          <a target="_blank" className="footer-link" href={ZK_IGNITE_LINK}>
+            zkIgnite
           </a>
         </div>
         <div className="flex flex-col">
           Connect
-          <a className="footer-link" href="http://google.com">
+          <a target="_blank" className="footer-link" href={DISCORD_LINK}>
             Discord
           </a>
-          <a className="footer-link" href="http://google.com">
-            Reddit
+          <a target="_blank" className="footer-link" href={TELEGRAM_LINK}>
+            Telegram
           </a>
-          <a className="footer-link" href="http://google.com">
-            Github
+          <a target="_blank" className="footer-link" href={SUPPORT_LINK}>
+            Support
           </a>
         </div>
       </div>
