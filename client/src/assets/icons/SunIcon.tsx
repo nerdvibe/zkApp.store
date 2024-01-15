34c98306833e9d283a6dc1b0c5679688dba6b3bd
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2024-01-15 22:01:53 +0100

    Fixed ts icons errors

--- client/src/assets/icons/SunIcon.tsx
+++ client/src/assets/icons/SunIcon.tsx
@@ -1,4 +1,4 @@
-export const SunIcon = (props) => (
+export const SunIcon = ({ className }: { className?: string }) => (
   <svg
     aria-hidden="true"
     focusable="false"
@@ -6,7 +6,7 @@ export const SunIcon = (props) => (
     role="presentation"
     viewBox="0 0 24 24"
     width="1em"
-    {...props}
+    className={className}
   >
     <g fill="currentColor">
       <path d="M19 12a7 7 0 11-7-7 7 7 0 017 7z" />
